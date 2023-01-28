254418d9d3bddd1f842b2e065c5d91ed7e5fb66c
Captain Unknown <captainunknown7@gmail.com>
2023-01-28 17:04:44 +0500

    Added Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+
+/* Pages */
 import Home from './pages/Home';
+import Login from './pages/Login/Login';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,9 +31,14 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
+
         <Route exact path="/home">
           <Home />
         </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>

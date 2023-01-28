7df66a2f4c74032a1b602bd04b6917f482536876
Captain Unknown <captainunknown7@gmail.com>
2023-01-28 22:27:46 +0500

    Added Profile Page

--- src/pages/Browse/Profile/Profile.tsx
+++ src/pages/Browse/Profile/Profile.tsx
@@ -1,20 +1,80 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Profile.css';
+import './Profile.scss';
+
+import { ReactComponent as UserAvatar } from "../../../assets/user.svg";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+          <div className='profileWrap'>
+              <div className='profileHeader'>
+                  <UserAvatar />
+                  <h2> User Name </h2>
+                  <h2 className='profileBalance'> Personal Balance: $40 </h2>
+              </div>
+              <div className='profileVolunteerMode'>
+                  <div className='profileVolunteerModeBox'>
+                  </div>
+                  <h2> Volunteer Mode </h2>
+                  <label className="switch">
+                      <input type="checkbox"/>
+                          <span className="slider round"></span>
+                  </label>
+              </div>
+              <div className='profileMenu'>
+                  <h1> My Chainj </h1>
+                  <div className='profileMyChainj'>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Saved Activities </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> My Interest </h2>
+                      </div>
+                  </div>
+                  <div className='profileMyChainj'>
+                      <h1> Chainjing </h1>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Manage Assignments </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Post a Request </h2>
+                      </div>
+                  </div>
+                  <div className='profileMyChainj'>
+                      <h1> General </h1>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Online Status </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Payments </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Invite Friends </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Support </h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </IonContent>
     </IonPage>
   );

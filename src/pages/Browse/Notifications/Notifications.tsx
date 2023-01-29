fed44fb23ee50f2841b31a7a2d07b3e354d47a4c
Captain Unknown <captainunknown7@gmail.com>
2023-01-29 18:55:27 +0500

    Added Assignment Component

--- src/pages/Browse/Notifications/Notifications.tsx
+++ src/pages/Browse/Notifications/Notifications.tsx
@@ -8,9 +8,27 @@ const Notifications: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
           <div className='notificationsWrap'>
+              <h1> Manage Assignments </h1>
+              {/* If request exists */}
+              <div className='notificationItem'>
+                  <img src='https://picsum.photos/200/200?random=1'/>
+                  <div className='notificationItemLeft'>
+                      <h2> Username </h2>
+                      <h1> Basic Accounting </h1>
+                      <div className='notificationTag'> Completed </div>
+                  </div>
+                  <div className='notificationItemRight'>
+                      <p> 1 hour ago </p>
+                      <p> Price $5 </p>
+                  </div>
+              </div>
+
+              {/* If request doesn't exist */}
+              {/*
               <NotFound/>
               <h1> No Request Found </h1>
               <a> Post a request </a>
+              */}
           </div>
       </IonContent>
     </IonPage>

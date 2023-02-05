d5274923a4f0981d90efbe281915fecfba24a0be
Captain Unknown <captainunknown7@gmail.com>
2023-02-05 18:36:11 +0500

    Added Categories & Interest Page

--- src/pages/Browse/Notifications/Notifications.tsx
+++ src/pages/Browse/Notifications/Notifications.tsx
@@ -1,79 +1,34 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Notifications.scss';
 
-import { ReactComponent as Close } from "../../../assets/close.svg";
-import { ReactComponent as UserAvatar } from "../../../assets/user.svg";
-import { ReactComponent as Search } from "../../../assets/search.svg";
+import { ReactComponent as NotFound } from "../../../assets/notfound.svg";
 
 const Notifications: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
           <div className='notificationsWrap'>
-              <div className='notificationsHeader'>
-                  <Close />
-                  <h2> Clients Requests </h2>
-                  <Search className='notificationsSearch'/>
-              </div>
-              <div className='notificationsTip'>
-                  <div className='notificationsUserInfo'>
-                      <UserAvatar />
-                      <div className='notificationsUserDetails'>
-                          <h3> John Doe </h3>
-                          <p> June 2, 2020 </p>
-                      </div>
+              <h1> Manage Assignments </h1>
+              {/* If request exists */}
+              <div className='notificationItem'>
+                  <img src='https://picsum.photos/200/200?random=1'/>
+                  <div className='notificationItemLeft'>
+                      <h2> Username </h2>
+                      <h1> Basic Accounting </h1>
+                      <div className='notificationTag'> Completed </div>
                   </div>
-                  <div className='notificationsRequestTitle'> Need someone to edit my photo </div>
-                  <div className='notificationsRequestTitle border'>Photoshop Editing / 23 Offers Sent </div>
-                  <div className='notificationsRequestDetail'>
-                      <div className='fill'></div>
-                      <div className='notificationsRequestDetailText'>
-                          <h2> Duration </h2>
-                          <h2> 2 Days </h2>
-                      </div>
-                  </div>
-                  <div className='notificationsRequestDetail'>
-                      <div className='fill'></div>
-                      <div className='notificationsRequestDetailText'>
-                          <h2> Budget </h2>
-                          <h2> $1 </h2>
-                      </div>
-                  </div>
-                  <div className='notificationsRequestDetail'>
-                      <div className='fill'></div>
-                      <div className='notificationsRequestDetailText'>
-                          <h2> Criteria </h2>
-                          <h2> Retouching </h2>
-                      </div>
+                  <div className='notificationItemRight'>
+                      <p> 1 hour ago </p>
+                      <p> Price <span>₵</span>5 </p>
                   </div>
               </div>
 
-              <button className='notificationSubmit'> Submit request </button>
-
-              { /* Popup */ }
-              <div className='notificationsPopupOverlay' style={{ visibility: 'visible' }}></div>
-              <div className='notificationsPopup' style={{ visibility: 'visible' }}>
-                  <h2> Filter </h2>
-                  <div className='notificationsFilterOptions'>
-                      <div className='notificationsFilterOptionItem'>
-                          <h2> All </h2>
-                          <input type='checkbox' id='notificationsFilterOptionItem1' />
-                      </div>
-                      <div className='notificationsFilterOptionItem'>
-                          <h2> Offered </h2>
-                          <input type='checkbox' id='notificationsFilterOptionItem1' />
-                      </div>
-                      <div className='notificationsFilterOptionItem'>
-                          <h2> Accounting </h2>
-                          <input type='checkbox' id='notificationsFilterOptionItem1' />
-                      </div>
-                      <div className='notificationsFilterOptionItem'>
-                          <h2> Fund Raising </h2>
-                          <input type='checkbox' id='notificationsFilterOptionItem1' />
-                      </div>
-                  </div>
-              </div>
-
+              {/* If request doesn't exist */}
+              {/*
+              <NotFound/>
+              <h1> No Request Found </h1>
+              <a> Post a request </a>
+              */}
           </div>
       </IonContent>
     </IonPage>
--- src/pages/Browse/Search/Search.tsx
+++ src/pages/Browse/Search/Search.tsx
@@ -1,58 +1,105 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Search.scss';
+import {Tabs, TabsProps} from "antd";
+
+import { ReactComponent as Back } from "../../../assets/arrowleft.svg";
+import { ReactComponent as SearchIcon } from "../../../assets/search.svg";
 
 const Search: React.FC = () => {
+    const items: TabsProps['items'] = [
+        {
+            key: '1',
+            label: `Categories`,
+            children: <>
+                <div className='searchTitle'>
+                    <h2> Your Interest </h2>
+                    <h3> Lorem Ipsum is simply dummy </h3>
+                </div>
+                <button> You may also like </button>
+                <h3 className='searchLike'> You may also like </h3>
+
+                <div className='searchResults'>
+                    <div className='searchResult'>
+                        <img src='https://picsum.photos/200/200?random=1' alt='searchResult' />
+                        <div className='searchResultInfo'>
+                            <h3> Basic Accounting </h3>
+                            <p> Lorem Ipsum is simply dummy </p>
+                        </div>
+                    </div>
+                    <div className='searchResult'>
+                        <img src='https://picsum.photos/200/200?random=2' alt='searchResult' />
+                        <div className='searchResultInfo'>
+                            <h3> Fund Raising </h3>
+                            <p> Lorem Ipsum is simply dummy </p>
+                        </div>
+                    </div>
+                    <div className='searchResult'>
+                        <img src='https://picsum.photos/200/200?random=3' alt='searchResult' />
+                        <div className='searchResultInfo'>
+                            <h3> Web Design </h3>
+                            <p> Lorem Ipsum is simply dummy </p>
+                        </div>
+                    </div>
+                    <div className='searchResult'>
+                        <img src='https://picsum.photos/200/200?random=4' alt='searchResult' />
+                        <div className='searchResultInfo'>
+                            <h3> Photography </h3>
+                            <p> Lorem Ipsum is simply dummy </p>
+                        </div>
+                    </div>
+                </div>
+            </>,
+        },
+        {
+            key: '2',
+            label: `Interests`,
+            children: <>
+                <div className='searchTitleUser'>
+                    <h3> Recently Viewed Users </h3>
+                    <a> Clear History </a>
+                </div>
+                <div className='searchResultsUser'>
+                    <div className='searchResult'>
+                        <div className='searchResultInfo'>
+                            <img src='https://picsum.photos/200/200?random=1' alt='searchResult' />
+                            <h3> Username </h3>
+                        </div>
+                    </div>
+                    <div className='searchResult'>
+                        <div className='searchResultInfo'>
+                            <img src='https://picsum.photos/200/200?random=2' alt='searchResult' />
+                            <h3> Username </h3>
+                        </div>
+                    </div>
+                    <div className='searchResult'>
+                        <div className='searchResultInfo'>
+                            <img src='https://picsum.photos/200/200?random=3' alt='searchResult' />
+                            <h3> Username </h3>
+                        </div>
+                    </div>
+                    <div className='searchResult'>
+                        <div className='searchResultInfo'>
+                            <img src='https://picsum.photos/200/200?random=4' alt='searchResult' />
+                            <h3> Username </h3>
+                        </div>
+                    </div>
+                </div>
+            </>,
+        },
+    ];
   return (
     <IonPage>
       <IonContent fullscreen>
           <div className='searchWrap'>
-              <h1> Categories </h1>
+              <div className='inboxHeader'>
+                  <Back />
+                  <h2> Categories </h2>
+              </div>
               <div className='searchSelectCategory'>
                   <h2> Categories </h2>
-                  <select name="interest" id="interest">
-                      <option value="volvo">Volvo</option>
-                      <option value="saab">Saab</option>
-                      <option value="mercedes">Mercedes</option>
-                      <option value="audi">Audi</option>
-                  </select>
+                  <SearchIcon />
               </div>
-              <div className='searchTitle'>
-                  <h2> Your Interest </h2>
-                  <h3> Lorem Ipsum is simply dummy </h3>
-              </div>
-              <button> You may also like </button>
-              <h3 className='searchLike'> You may also like </h3>
-
-              <div className='searchResults'>
-                  <div className='searchResult'>
-                      <img src='https://picsum.photos/200/200?random=1' alt='searchResult' />
-                      <div className='searchResultInfo'>
-                          <h3> Basic Accounting </h3>
-                          <p> Lorem Ipsum is simply dummy </p>
-                      </div>
-                  </div>
-                  <div className='searchResult'>
-                      <img src='https://picsum.photos/200/200?random=2' alt='searchResult' />
-                      <div className='searchResultInfo'>
-                          <h3> Fund Raising </h3>
-                          <p> Lorem Ipsum is simply dummy </p>
-                      </div>
-                  </div>
-                  <div className='searchResult'>
-                      <img src='https://picsum.photos/200/200?random=3' alt='searchResult' />
-                      <div className='searchResultInfo'>
-                          <h3> Web Design </h3>
-                          <p> Lorem Ipsum is simply dummy </p>
-                      </div>
-                  </div>
-                  <div className='searchResult'>
-                      <img src='https://picsum.photos/200/200?random=4' alt='searchResult' />
-                      <div className='searchResultInfo'>
-                          <h3> Photography </h3>
-                          <p> Lorem Ipsum is simply dummy </p>
-                      </div>
-                  </div>
-              </div>
+              <Tabs defaultActiveKey="1" items={items} style={{ width: '90%' }} centered={true} />
           </div>
       </IonContent>
     </IonPage>

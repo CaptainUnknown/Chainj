3f67eeb94afa87e50c30faefc1c7df7ffd146839
CaptainUnknown <captainunknown7@gmail.com>
2023-02-12 00:02:35 +0500

    Changes in Profile & Homepage

--- src/pages/Browse/Profile/Profile.tsx
+++ src/pages/Browse/Profile/Profile.tsx
@@ -1,77 +1,91 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import './Profile.scss';
 
 import { ReactComponent as UserAvatar } from "../../../assets/user.svg";
+import { ReactComponent as Back } from "../../../assets/arrowleft.svg";
 
 const Home: React.FC = () => {
+    const data = [
+        {name: '1', uv: 40, pv: 2400, amt: 2400},
+        {name: '2', uv: 60, pv: 2400, amt: 2400},
+        {name: '3', uv: 120, pv: 2400, amt: 2400},
+        {name: '4', uv: 90, pv: 2400, amt: 2400},
+        {name: '5', uv: 100, pv: 2400, amt: 2400},
+        {name: '6', uv: 95, pv: 2400, amt: 2400}
+    ];
+
   return (
     <IonPage>
 
       <IonContent fullscreen>
           <div className='profileWrap'>
-              <div className='profileHeader'>
-                  <UserAvatar />
-                  <h2> User Name </h2>
-                  <h2 className='profileBalance'> Personal Balance: $40 </h2>
+              <div className='inboxHeader'>
+                  <Back />
+                  <h2 style={{ marginBottom: '0', marginLeft: '20px' }}> Earnings </h2>
               </div>
-              <div className='profileVolunteerMode'>
-                  <div className='profileVolunteerModeBox'>
-                  </div>
-                  <h2> Volunteer Mode </h2>
-                  <label className="switch">
-                      <input type="checkbox"/>
-                          <span className="slider round"></span>
-                  </label>
+              <h1 style={{ marginTop: '2rem' }}> ¢50 </h1>
+              <h2> Personal Balance </h2>
+              <div className='earningsWrap'>
+                  <h2> Overview </h2>
+                  <h2 style={{ marginLeft: 'auto' }}> May 28 - June 03 </h2>
               </div>
-              <div className='profileMenu'>
-                  <h1> My Chainj </h1>
-                  <div className='profileMyChainj'>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Saved Activities </h2>
-                      </div>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> My Interest </h2>
-                      </div>
+
+              <div className='graphWrap'>
+                  <LineChart width={600} height={300} data={data}>
+                      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                      <CartesianGrid stroke="#ccc" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                  </LineChart>
+              </div>
+
+              <div className='stats'>
+                  <div className='heading'> Analytics </div>
+                  <div className='stat'>
+                      <h2> Earning from June </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $120 </h2>
                   </div>
-                  <div className='profileMyChainj'>
-                      <h1> Chainjing </h1>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Manage Assignments </h2>
-                      </div>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Post a Request </h2>
-                      </div>
+                  <div className='stat'>
+                      <h2> Avg Selling Price </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
                   </div>
-                  <div className='profileMyChainj'>
-                      <h1> General </h1>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Online Status </h2>
-                      </div>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Payments </h2>
-                      </div>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Invite Friends </h2>
-                      </div>
-                      <div className='profileMenuItem'>
-                          <div className='profileVolunteerModeBox'>
-                          </div>
-                          <h2> Support </h2>
-                      </div>
+                  <div className='stat'>
+                      <h2> Earning from June </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $120 </h2>
+                  </div>
+                  <div className='stat'>
+                      <h2> Active Assignments </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='stat'>
+                      <h2> Available for withdrawal </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='stat'>
+                      <h2> Completed Assignments </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='heading'> Revenues </div>
+                  <div className='stat' style={{ borderBottom: '2px solid var(--bg-accent)'}}>
+                      <h2> Cancelled Assignments </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='stat' style={{ borderBottom: '2px solid var(--bg-accent)'}}>
+                      <h2> Pending Clearance </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='stat' style={{ borderBottom: '2px solid var(--bg-accent)'}}>
+                      <h2> Withdrawn </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='stat' style={{ borderBottom: '2px solid var(--bg-accent)'}}>
+                      <h2> Used for Purchases </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
+                  </div>
+                  <div className='stat' style={{ borderBottom: '2px solid var(--bg-accent)'}}>
+                      <h2> Cleared </h2>
+                      <h2 style={{ marginLeft: 'auto' }}> $50 </h2>
                   </div>
               </div>
           </div>
---
+++ src/pages/Browse/Profile/ProfileDefault.tsx
@@ -0,0 +1,83 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Profile.scss';
+
+import { ReactComponent as UserAvatar } from "../../../assets/user.svg";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+
+      <IonContent fullscreen>
+          <div className='profileWrap'>
+              <div className='profileHeader'>
+                  <UserAvatar />
+                  <h2> User Name </h2>
+                  <h2 className='profileBalance'> Personal Balance: ¢40 </h2>
+              </div>
+              <div className='profileVolunteerMode'>
+                  <div className='profileVolunteerModeBox'>
+                  </div>
+                  <h2> Volunteer Mode </h2>
+                  <label className="switch">
+                      <input type="checkbox"/>
+                          <span className="slider round"></span>
+                  </label>
+              </div>
+              <div className='profileMenu'>
+                  <h1> My Chainj </h1>
+                  <div className='profileMyChainj'>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Saved Activities </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> My Interest </h2>
+                      </div>
+                  </div>
+                  <div className='profileMyChainj'>
+                      <h1> Chainjing </h1>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Manage Assignments </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Post a Request </h2>
+                      </div>
+                  </div>
+                  <div className='profileMyChainj'>
+                      <h1> General </h1>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Online Status </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Payments </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Invite Friends </h2>
+                      </div>
+                      <div className='profileMenuItem'>
+                          <div className='profileVolunteerModeBox'>
+                          </div>
+                          <h2> Support </h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;

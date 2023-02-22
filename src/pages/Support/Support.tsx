8ff1c79291b9d435bfe35715c46368cd7ddaa045
CaptainUnknown <79987654+CaptainUnknown@users.noreply.github.com>
2023-02-22 17:29:52 +0500

    Removed Unused Imports

--- src/pages/Support/Support.tsx
+++ src/pages/Support/Support.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import './Support.scss';
 
 import { ReactComponent as Close } from "../../assets/arrowleft.svg";
--- src/pages/Terms/Terms.tsx
+++ src/pages/Terms/Terms.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import './Terms.scss';
 
 import { ReactComponent as Back } from "../../assets/arrowleft.svg";
